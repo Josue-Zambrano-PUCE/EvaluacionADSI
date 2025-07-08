3651e5c672e885fdacc10593ce22dd1ab11d2a80
Josue Zambrano <jjzambranoz@puce.edu.ec>
2025-07-08 17:41:23 -0500

    Inicializar repositorio

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('saludoBtn').addEventListener('click', () => {
+    const mensaje = "¡Hola! Esta es mi Evaluacion Parcial 2 de ADSI.";
+    document.getElementById('saludoMsg').textContent = mensaje;
+});
